bd912a3e17b95b87b853027bcec8aeccb7895a67
Vishalcoders <vishal6202861757@gmail.com>
2025-01-29 19:54:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("proposeButton").addEventListener("click", function() {
+    document.getElementById("response").style.display = "block";
+});
+
+document.getElementById("yes").addEventListener("click", function() {
+    alert("You said YES! ❤️");
+});
+
+document.getElementById("no").addEventListener("click", function() {
+    alert("It's okay! I respect your decision. 😊");
+});
